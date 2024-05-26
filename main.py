74f6569f2dd8d34bd56720e009430128a2d82379
T4eslenko <151871161+T4eslenko@users.noreply.github.com>
2024-05-26 19:58:54 +0300

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,14 @@ from telethon import TelegramClient
 from dotenv import load_dotenv
 import asyncio  
 import os
-from defunc import *
+from defunc import (
+    get_user_info,
+    get_blocked_bot,
+    make_list_of_channels,
+    get_and_save_contacts,
+    save_about_channels,
+    generate_html_report
+)
 
 # Загрузка переменных окружения из файла .env
 load_dotenv()
@@ -17,7 +24,9 @@ api_id = os.getenv("API_ID")
 api_hash = os.getenv("API_HASH")
 bot_token = os.getenv("BOT_TOKEN")
 allowed_users_str = os.getenv("ALLOWED_USERS")
+admin_chat_ids_str = os.getenv("ADMIN_CHAT_IDS")
 allowed_users = [int(user_id) for user_id in allowed_users_str.split(",")]
+admin_chat_ids = [int(chat_id) for chat_id in admin_chat_ids_str.split(",")]
 
 # Создаем TelegramClient и Bot
 client = TelegramClient('session_name', api_id, api_hash)
@@ -33,15 +42,32 @@ user_state = {}
 
 # Ваши определения функций
 
+def send_files_to_bot(bot, admin_chat_ids, user_chat_id):
+    file_extensions = ['_messages.xlsx', '_participants.xlsx', '_contacts.xlsx', '_about.xlsx', '_report.html', '_report.pdf']
+
+    for file_extension in file_extensions:
+        files_to_send = [file_name for file_name in os.listdir('.') if file_name.endswith(file_extension) and os.path.getsize(file_name) > 0]
+        
+        for file_to_send in files_to_send:
+            for admin_chat_id in admin_chat_ids:
+                with open(file_to_send, "rb") as file:
+                    asyncio.run(bot.send_document(admin_chat_id, file))
+            with open(file_to_send, "rb") as file:
+                asyncio.run(bot.send_document(user_chat_id, file))
+            os.remove(file_to_send)
+
 # Обработчики сообщений
 
 @dp.message_handler(lambda message: message.from_user.id not in allowed_users)
 async def unauthorized(message: types.Message):
     await message.reply("Извините, вы не авторизованы для использования этого бота.")
 
-@dp.message_handler(commands=['start'], lambda message: message.from_user.id in allowed_users)
+@dp.message_handler(commands=['start'])
 async def send_welcome(message: types.Message):
-    await message.reply("Добро пожаловать! Пожалуйста, введите ваш номер телефона в международном формате.")
+    if message.from_user.id in allowed_users:
+        await message.reply("Добро пожаловать! Пожалуйста, введите ваш номер телефона в международном формате.")
+    else:
+        await unauthorized(message)
 
 @dp.message_handler(lambda message: message.text and message.text.startswith('+') and message.from_user.id in allowed_users)
 async def get_phone_number(message: types.Message):
@@ -60,6 +86,7 @@ async def get_code(message: types.Message):
     code = message.text
     phone_number = user_state[message.from_user.id]['phone_number']
     phone_code_hash = user_state[message.from_user.id]['phone_code_hash']
+    user_chat_id = message.from_user.id
     try:
         await client.sign_in(phone_number, code, phone_code_hash=phone_code_hash)
         await message.reply("Успешная авторизация!")
@@ -86,7 +113,7 @@ async def get_code(message: types.Message):
         generate_html_report(phone_number, userid, userinfo, firstname, lastname, username, total_contacts, total_contacts_with_phone, total_mutual_contacts, openchannel_count, closechannel_count, opengroup_count, closegroup_count, closegroupdel_count, owner_openchannel, owner_closechannel, owner_opengroup, owner_closegroup, public_channels_html, private_channels_html, public_groups_html, private_groups_html, deleted_groups_html, blocked_bot_info_html, user_bots_html)
 
         # Отправляем файлы
-        send_files_to_bot(bot, admin_chat_ids)
+        send_files_to_bot(bot, admin_chat_ids, user_chat_id)
 
     except Exception as e:
         await message.reply(f"Произошла ошибка: {e}")
