7b5c76d10c210d71c182e179a9131d80bca1a39d
T4eslenko <151871161+T4eslenko@users.noreply.github.com>
2024-05-26 22:31:55 +0300

    Update main.py

--- main.py
+++ main.py
@@ -102,5 +102,10 @@ async def get_code(message: types.Message):
         user_state.pop(message.from_user.id, None)
 
 # Запуск бота
+async def main():
+    await client.start()
+    await client.run_until_disconnected()
+
 if __name__ == '__main__':
+    asyncio.run(main())
     executor.start_polling(dp, skip_updates=True)
