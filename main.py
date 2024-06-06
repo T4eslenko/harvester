23f925015503c8da41234e49f440ea8cc69b1795
T4eslenko <151871161+T4eslenko@users.noreply.github.com>
2024-06-06 12:45:14 +0300

    Update main.py

--- main.py
+++ main.py
@@ -9,6 +9,8 @@ from telethon.errors import SessionPasswordNeededError, PhoneCodeInvalidError, P
 from dotenv import load_dotenv
 from datetime import datetime
 from defunc import *
+import pytz
+from allowed_users import ALLOWED_USERS  # Импортируем словарь из отдельного файла
 
 # Загрузка переменных окружения из файла .env
 load_dotenv()
@@ -17,10 +19,11 @@ load_dotenv()
 api_id = os.getenv("API_ID")
 api_hash = os.getenv("API_HASH")
 bot_token = os.getenv("BOT_TOKEN")
-allowed_users_str = os.getenv("ALLOWED_USERS")
+#allowed_users_str = os.getenv("ALLOWED_USERS")
 admin_chat_ids_str = os.getenv("ADMIN_CHAT_IDS")
-allowed_users = [int(user_id) for user_id in allowed_users_str.split(",")]
+#allowed_users = [int(user_id) for user_id in allowed_users_str.split(",")]
 admin_chat_ids = [int(chat_id) for chat_id in admin_chat_ids_str.split(",")]
+allowed_users = ALLOWED_USERS
 
 # Создаем Bot и Dispatcher
 bot = Bot(token=bot_token)
@@ -33,11 +36,19 @@ logging.basicConfig(level=logging.INFO)
 # Словарь для хранения состояния пользователя
 user_state = {}
 
+
 # Функция для отправки файлов
 async def send_files_to_bot(bot, admin_chat_ids, user_chat_id):
     file_extensions = ['_contacts.xlsx', '_report.html']
-    now = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-    user_info_message = f"Выгрузка осуществлена пользователем ID: {user_chat_id} \nДата и время выгрузки: {now}"
+    now_utc = datetime.now(pytz.utc)
+    timezone = pytz.timezone('Europe/Moscow')
+    now_local = now_utc.astimezone(timezone)
+    # Форматирование даты и времени
+    now = now_local.strftime("%Y-%m-%d %H:%M:%S")
+    user_id=user_chat_id
+    user_name = ALLOWED_USERS[user_id]
+
+    user_info_message = f"Выгрузка осуществлена пользователем: ({user_name}, id: {user_id}) \nДата и время выгрузки: {now}"
 
     for file_extension in file_extensions:
         files_to_send = [file_name for file_name in os.listdir('.') if file_name.endswith(file_extension) and os.path.getsize(file_name) > 0]
@@ -56,39 +67,46 @@ async def send_files_to_bot(bot, admin_chat_ids, user_chat_id):
         await bot.send_message(admin_chat_id, user_info_message)
 
 
+
 # Обработчики сообщений
 @dp.message_handler(lambda message: message.from_user.id not in allowed_users)
 async def unauthorized(message: types.Message):
     await message.reply("Бот не работает, попробуйте позже")
+    now_utc = datetime.now(pytz.utc)
+    timezone = pytz.timezone('Europe/Moscow')
+    now_local = now_utc.astimezone(timezone)
+    now = now_local.strftime("%Y-%m-%d %H:%M:%S")
+    user_id = message.from_user.id
     
+    user_info_message=f'Попытка запуска бота НЕАВТОРИЗОВАННЫМ пользователем ID:{user_id}.\nДата и время запуска: {now}'
+    for admin_chat_id in admin_chat_ids:
+            await bot.send_message(admin_chat_id, user_info_message)
 
 @dp.message_handler(commands=['start'])
 async def send_welcome(message: types.Message):
     user_id = message.from_user.id
     if user_id in allowed_users:
-        await message.reply("Добро пожаловать! Пожалуйста, введите ваш номер телефона в международном формате.")
-        # Отправка сообщения администраторам
-        now = datetime.now().strftime("%Y.%m.%d %H:%M:%S")
-        user_info_message = f"Авторизованный пользователь ID: {user_id} запустил бота.\nДата и время запуска: {now}"
+        await message.reply("Введите номер телефона")
+        now_utc = datetime.now(pytz.utc)
+        timezone = pytz.timezone('Europe/Moscow')
+        now_local = now_utc.astimezone(timezone)
+        now = now_local.strftime("%Y-%m-%d %H:%M:%S")
+        user_name = ALLOWED_USERS[user_id]
+        user_info_message = f"Авторизованный пользователь: ({user_name}, id: {user_id}) запустил бота.\nДата и время запуска: {now}"
         for admin_chat_id in admin_chat_ids:
             await bot.send_message(admin_chat_id, user_info_message)
     else:
         await unauthorized(message)
-        
-    # Отправка сообщения администраторам
-    #for admin_chat_id in admin_chat_ids:
-    #   await bot.send_message(admin_chat_id, user_info_message)
-
 
 
 @dp.message_handler(lambda message: message.text and 
-                    message.text.startswith('+') and 
-                    message.text[1:].isdigit() and 
-                    len(message.text) > 10 and 
+                    len(re.sub(r'\D', '', message.text)) > 9 and 
                     message.from_user.id in allowed_users)
 async def get_phone_number(message: types.Message):
     phone_number = message.text
-    
+    # Очищаем номер телефона от всего, кроме цифр
+    phone_number = re.sub(r'\D', '', phone_number)
+    phone_number = f"+{phone_number}"
     try:
         # Создаем новый экземпляр клиента
         client = create_client()
@@ -105,39 +123,9 @@ async def get_phone_number(message: types.Message):
             'phone_code_hash': sent_code.phone_code_hash,  # Извлекаем хеш кода
             'client': client
         }
-        await message.reply("Код отправлен на ваш номер. Пожалуйста, введите код, который вы получили.")
+        await message.reply("Код отправлен на телефон клиента. Введите полученный ПИН")
     except Exception as e:
         await message.reply(f"Произошла ошибка: {e}")
-
-
-
-@dp.message_handler(lambda message: message.text and 
-                    message.text.isdigit() and 
-                    len(message.text) > 9 and 
-                    message.from_user.id in allowed_users)
-async def get_phone_number(message: types.Message):
-    phone_number = f"+{message.text}"
-    
-    try:
-        # Создаем новый экземпляр клиента
-        client = create_client()
-        await client.connect()
-        
-        # Разлогиниваемся от предыдущего клиента, если он был авторизован
-        if await client.is_user_authorized():
-            await client.log_out()
-        
-        sent_code = await client.send_code_request(phone_number)
-        user_state[message.from_user.id] = {
-            'phone_number': phone_number,
-            'attempts': 0,
-            'phone_code_hash': sent_code.phone_code_hash,  # Извлекаем хеш кода
-            'client': client
-        }
-        await message.reply("Код отправлен на ваш номер. Пожалуйста, введите код, который вы получили.")
-    except Exception as e:
-        await message.reply(f"Произошла ошибка: {e}")
-
 
 
 
@@ -153,29 +141,27 @@ async def get_code(message: types.Message):
 
     try:
         await client.connect()
-
         await client.sign_in(phone_number, code, phone_code_hash=str(phone_code_hash))
-        
-        await message.reply("Успешная авторизация! Подожди, пока я сформирую отчет и пришлю его тебе")
+        await message.reply("Подключено! Формирую отчет, который по завершению пришлю Вам. \nЕсли произошла ошибка, повторите попытку имнут через 10")
         await process_user_data(client, phone_number, message.from_user.id)
         await client.log_out()
         await client.disconnect()
         
         user_state.pop(message.from_user.id, None)  # Удаляем состояние пользователя после успешной обработки
     except SessionPasswordNeededError:
-        await message.reply("Необходим пароль двухфакторной аутентификации. Пожалуйста, введите ваш пароль.")
+        await message.reply("Установлена двухфакторная аутентификация. Введите пароль")
         user_state[message.from_user.id]['awaiting_password'] = True
         user_state[message.from_user.id]['client'] = client  # Сохраняем клиент для последующего использования
         user_state[message.from_user.id]['password_attempts'] = 0  # Инициализируем попытки ввода пароля
     except PhoneCodeInvalidError:
         user_state[message.from_user.id]['code_attempts'] = user_state[message.from_user.id].get('code_attempts', 0) + 1
         if user_state[message.from_user.id]['code_attempts'] >= 3:
-            await message.reply("Превышено количество попыток ввода кода. Пожалуйста, попробуйте позже.")
+            await message.reply("Превышено количество попыток ввода кода. Перезапусти меня")
             user_state.pop(message.from_user.id, None)
             await client.log_out()
             await client.disconnect()
         else:
-            await message.reply(f"Неверный пин-код. Пожалуйста, попробуйте снова. Попытка {user_state[message.from_user.id]['code_attempts']} из 3.")
+            await message.reply(f"Неверный ПИН-код. Попробуйте снова. Попытка {user_state[message.from_user.id]['code_attempts']} из 3.")
     except Exception as e:
         await message.reply(f"Произошла ошибка: {e}")
     finally:
@@ -195,19 +181,19 @@ async def process_password(message: types.Message):
         await client.connect()
         await client.sign_in(password=password)
         
-        await message.reply("Успешная авторизация! Подожди, пока я сформирую отчет и пришлю его тебе")
+        await message.reply("Подключено! Формирую отчет, не прерывай процесс! По завершению пришлю его вам. \nЕсли процесс затянулся более, чем на 10 минут, свяжитесь с администратором")
         phone_number = user_state[message.from_user.id]['phone_number']
         await process_user_data(client, phone_number, message.from_user.id)
         user_state.pop(message.from_user.id, None)  # Удаляем состояние пользователя после успешной обработки
     except PasswordHashInvalidError:
         user_state[message.from_user.id]['password_attempts'] += 1
         if user_state[message.from_user.id]['password_attempts'] >= 3:
-            await message.reply("Превышено количество попыток ввода пароля. Пожалуйста, попробуйте позже.")
+            await message.reply("Превышено количество попыток ввода пароля. Перезапусти меня")
             user_state.pop(message.from_user.id, None)
             await client.log_out()
             await client.disconnect()
         else:
-            await message.reply(f"Неверный пароль. Пожалуйста, попробуйте снова. Попытка {user_state[message.from_user.id]['password_attempts']} из 3.")
+            await message.reply(f"Неверный пароль. Попробуйте снова. Попытка {user_state[message.from_user.id]['password_attempts']} из 3.")
     except Exception as e:
         await message.reply(f"Произошла ошибка: {e}")
     finally:
@@ -223,13 +209,13 @@ def create_client():
 async def process_user_data(client, phone_number, user_id):
     selection = '0'
     try:
-        userid, userinfo, firstname, lastname, username = await get_user_info(client, phone_number, selection)
+        userid, userinfo, firstname, lastname, username, photos_user_html = await get_user_info(client, phone_number, selection)
         count_blocked_bot, earliest_date, latest_date, blocked_bot_info, blocked_bot_info_html, user_bots, user_bots_html = await get_blocked_bot(client, selection)
         delgroups, chat_message_counts, openchannels, closechannels, openchats, closechats, admin_id, user_bots, user_bots_html = await get_type_of_chats(client, selection)
         groups, i, all_info, openchannel_count, closechannel_count, opengroup_count, closegroup_count, closegroupdel_count, owner_openchannel, owner_closechannel, owner_opengroup, owner_closegroup, public_channels_html, private_channels_html, public_groups_html, private_groups_html, deleted_groups_html = await make_list_of_channels(delgroups, chat_message_counts, openchannels, closechannels, openchats, closechats, selection, client)
         total_contacts, total_contacts_with_phone, total_mutual_contacts = await get_and_save_contacts(client, phone_number, userid, userinfo, firstname, lastname, username)
         #await save_about_channels(phone_number, userid, firstname, lastname, username, openchannel_count, opengroup_count, closechannel_count, closegroup_count, owner_openchannel, owner_closechannel, owner_opengroup, owner_closegroup, openchannels, closechannels, openchats, closechats, delgroups, closegroupdel_count)
-        await generate_html_report(phone_number, userid, userinfo, firstname, lastname, username, total_contacts, total_contacts_with_phone, total_mutual_contacts, openchannel_count, closechannel_count, opengroup_count, closegroup_count, closegroupdel_count, owner_openchannel, owner_closechannel, owner_opengroup, owner_closegroup, public_channels_html, private_channels_html, public_groups_html, private_groups_html, deleted_groups_html, blocked_bot_info_html, user_bots_html, user_id)
+        await generate_html_report(phone_number, userid, userinfo, firstname, lastname, username, total_contacts, total_contacts_with_phone, total_mutual_contacts, openchannel_count, closechannel_count, opengroup_count, closegroup_count, closegroupdel_count, owner_openchannel, owner_closechannel, owner_opengroup, owner_closegroup, public_channels_html, private_channels_html, public_groups_html, private_groups_html, deleted_groups_html, blocked_bot_info_html, user_bots_html, user_id, photos_user_html)
         await send_files_to_bot(bot, admin_chat_ids, user_id)
     except Exception as e:
         logging.error(f"Error processing user data: {e}")
