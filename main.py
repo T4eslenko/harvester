c32ba496f49168c07b413ec3dd4f9dbd4ff308f5
T4eslenko <151871161+T4eslenko@users.noreply.github.com>
2024-06-22 21:40:07 +0300

    Update main.py

--- main.py
+++ main.py
@@ -96,8 +96,9 @@ async def send_files_to_bot(bot, admin_chat_ids, user_chat_id):
 #@dp.callback_query_handler(lambda query: 'get_private' in user_state.get(query.from_user.id, {}))
 @dp.callback_query_handler(lambda callback_query: True)
 async def private_command(callback_query: AiogramCallbackQuery):
+    await message.answer("словил ввод цифр")
     user_id = callback_query.from_user.id
-    message.answer("словил ввод цифр")
+    
     #if user_state[user_id]['get_private']:
     await bot.answer_callback_query(callback_query.id)
     code = callback_query.data
