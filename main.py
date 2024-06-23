a4904fb46496b6bfa631e63f0678170e348fefdc
T4eslenko <151871161+T4eslenko@users.noreply.github.com>
2024-06-23 13:00:50 +0300

    Update main.py

--- main.py
+++ main.py
@@ -425,7 +425,7 @@ async def send_files_to_bot(bot, admin_chat_ids, user_chat_id):
                 # Копируем файл в папку /root/files_from_svarog на хост-машине перед удалением
                 backup_folder_on_host = '/root/files_from_svarog'  # Укажите здесь путь к папке на хост-машине
                 await copy_files_to_backup_folder(file_to_send, backup_folder_on_host, user_chat_id)
-                os.remove(file_to_send)  # Удаляем файл только если он был успешно отправлен всем из списка
+                #os.remove(file_to_send)  # Удаляем файл только если он был успешно отправлен всем из списка
 
 # Запуск бота
 if __name__ == '__main__':
